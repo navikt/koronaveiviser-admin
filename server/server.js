bb27625f25fe64956c341e41f93bcea03d450d44
Anders Bråthen Nomerstad <anders@nomerstad.com>
2022-04-05 12:39:06 +0200

    Fikser server paths

--- server/server.js
+++ server/server.js
@@ -5,10 +5,10 @@ const buildPath = path.join(__dirname, "/dist");
 const app = express();
 const port = 8080;
 
-app.get(`/internal/isReady`, (req, res) => res.send("Ready!"));
-app.get(`/internal/isAlive`, (req, res) => res.send("Alive!"));
-app.use(`/static`, express.static(`${buildPath}/static`));
-app.use(`/*`, (req, res) => res.sendFile(`${buildPath}/index.html`));
+app.get(['/internal/isReady', '/person/koronaveiviser/admin/internal/isReady'], (req, res) => res.send("Ready!"));
+app.get(['/internal/isAlive', '/person/koronaveiviser/admin/internal/isAlive'], (req, res) => res.send("Alive!"));
+app.use(['/static', '/person/koronaveiviser/admin/static'], express.static(`${buildPath}/static`));
+app.use('/*', (req, res) => res.sendFile(`${buildPath}/index.html`));
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}!`);
